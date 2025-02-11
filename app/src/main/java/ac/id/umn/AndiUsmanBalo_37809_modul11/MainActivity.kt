0979566c1202263ade5ee65aa90abc136a5bfe49
Andi Usman Balo <andibalo213@gmail.com>
2025-02-11 09:25:49 +0700

    State in Compose

--- app/src/main/java/ac/id/umn/AndiUsmanBalo_37809_modul11/MainActivity.kt
+++ app/src/main/java/ac/id/umn/AndiUsmanBalo_37809_modul11/MainActivity.kt
@@ -18,6 +18,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
@@ -43,19 +45,28 @@ fun MyApp(modifier: Modifier = Modifier, names : List<String> = listOf("World", 
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val expanded = remember {
+        mutableStateOf(false)
+    }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.weight(1f)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
                 Text(text = "Hello ")
                 Text(text = name)
             }
             ElevatedButton(
-                onClick = { /* TODO */ }
+                onClick = { expanded.value = !expanded.value }
             ) {
-                Text("Show more")
+                Text(if (expanded.value) "Show less" else "Show more")
             }
         }
     }
